a82277550ce624b84b98044f9712a243d2883d56
Dennis Pettersson <dennis.pettersson@iteam.se>
2020-04-08 09:53:58 +0200

    feat: showing how import/export can be used

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,9 @@
 import { Application } from 'express'
 
+import { routes as tenantRoutes } from './services/tenantservice'
+
+export { tenantRoutes }
+
 export default (app: Application) => {
   app.get('/', (_req, res) => res.send('fastAPI'))
 }
