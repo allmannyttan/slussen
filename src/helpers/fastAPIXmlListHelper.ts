bb87eb535a745e1b24e19b5a1b512444003525b9
martinloman <martin.loman@gmail.com>
2020-04-15 15:07:56 +0200

    chore: create TS interfaces

--- src/helpers/fastAPIXmlListHelper.ts
+++ src/helpers/fastAPIXmlListHelper.ts
@@ -1,33 +1,36 @@
-
-
-import { Fi2ClassNode }  from '@app/types'
+import { Fi2ClassNode, Fi2ClassListXml } from './types'
 import fs from 'fs'
 import xml2js from 'xml2js'
- 
 
 export const getNameFromClasslist = async (node: Fi2ClassNode) => {
-  if (!node.fi2class_code)
-    return ''  //Silently return empty string or throw error?
+  if (!node.fi2class_code) return '' //Silently return empty string or throw error?
 
   const code = node.fi2class_code[0]
-  
+
   const xml = fs.readFileSync(
     `src/fastAPIXml/classlists/${node.fi2class_scheme[0].fi2scheme_id}.xml`,
-    { encoding: 'utf8'})
+    { encoding: 'utf8' }
+  )
 
-  if (!code || !xml)
-    return ''
+  if (!code || !xml) return ''
 
-  const json = await xml2js.parseStringPromise(xml)
+  const json: Fi2ClassListXml = await xml2js.parseStringPromise(xml)
 
-  if(json.document.validationlist) {
-    const item = json.document.validationlist[0]["list-item"].find(element => element['item-value'][0] === code)
-    return item['item-description'][0]
+  if (json.document.validationlist) {
+    const item = json.document.validationlist[0]['list-item'].find(
+      (element) => element['item-value'][0] === code
+    )
+
+    return item &&
+      item['item-description'] &&
+      Array.isArray(item['item-description'])
+      ? item['item-description'][0]
+      : ''
   }
-  
+
   return ''
 }
 
 export default {
-  getNameFromClasslist
+  getNameFromClasslist,
 }
---
+++ src/helpers/types.ts
@@ -0,0 +1,61 @@
+/********** Fi2XML types **********/
+
+export interface Fi2SchemeNode {
+  fi2scheme_id: string[]
+  fi2scheme_name: string[]
+  fi2scheme_url: string[]
+}
+
+export interface Fi2ClassNode {
+  fi2class_code: string[]
+  fi2class_scheme: Fi2SchemeNode[]
+}
+
+export interface Namespace {
+  'xmlns:xsi': string
+  'xsi:noNamespaceSchemaLocation': string
+}
+
+export interface Author {
+  name: string[]
+  company: string[]
+}
+
+export interface Metadata {
+  identity: string[]
+  description: string[]
+  version: string[]
+  date: string[]
+  author: Author[]
+  reference: string[]
+}
+
+export interface ListItem2 {
+  'item-value': string[]
+  'item-description': string[]
+  'item-definition': string[]
+}
+
+export interface ListItem {
+  'item-value': string[]
+  'item-description': string[]
+  'item-definition': string[]
+  'list-item': ListItem2[]
+}
+
+export interface Validationlist {
+  'list-value': string[]
+  'list-description': string[]
+  'list-definition': string[]
+  'list-item': ListItem[]
+}
+
+export interface Document {
+  $: Namespace
+  metadata: Metadata[]
+  validationlist: Validationlist[]
+}
+
+export interface Fi2ClassListXml {
+  document: Document
+}
