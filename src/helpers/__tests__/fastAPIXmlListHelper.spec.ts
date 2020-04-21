61a52f2fa5b39bb51268a684a49c560032801489
martinloman <martin.loman@gmail.com>
2020-04-21 15:12:17 +0200

    wip: change xml2json package

--- src/helpers/__tests__/fastAPIXmlListHelper.spec.ts
+++ src/helpers/__tests__/fastAPIXmlListHelper.spec.ts
@@ -5,14 +5,12 @@ import { ListItem } from '../types'
 describe('#fastAPIXmlListHelper', () => {
   test('gets name from class list', async () => {
     const node = {
-      fi2class_code: ['16'],
-      fi2class_scheme: [
-        {
-          fi2scheme_id: ['CA000_005_003'],
-          fi2scheme_name: ['Klassificering FI2Partner'],
-          fi2scheme_url: ['http://www.fi2.se/classlist/CA000_005_003.xml'],
-        },
-      ],
+      fi2class_code: '16',
+      fi2class_scheme: {
+        fi2scheme_id: 'CA000_005_003',
+        fi2scheme_name: 'Klassificering FI2Partner',
+        fi2scheme_url: 'http://www.fi2.se/classlist/CA000_005_003.xml',
+      },
     }
     const name = await helper.getNameFromClasslist(node)
     expect(name).toEqual('Hyresgäst')
@@ -20,14 +18,12 @@ describe('#fastAPIXmlListHelper', () => {
 
   test('gets name from value list', async () => {
     const node = {
-      fi2value_code: ['Rooms'],
-      fi2value_scheme: [
-        {
-          fi2scheme_id: ['VB001_003_001'],
-          fi2scheme_name: ['Tillåtna värden på ett utrymmessystem'],
-          fi2scheme_url: ['http://www.fi2.se/valuelist/VB001_003_001.xml'],
-        },
-      ],
+      fi2value_code: 'Rooms',
+      fi2value_scheme: {
+        fi2scheme_id: 'VB001_003_001',
+        fi2scheme_name: 'Tillåtna värden på ett utrymmessystem',
+        fi2scheme_url: 'http://www.fi2.se/valuelist/VB001_003_001.xml',
+      },
     }
     const name = await helper.getNameFromValuelist(node)
     expect(name).toEqual('Antal rum')
@@ -37,66 +33,66 @@ describe('#fastAPIXmlListHelper', () => {
     test('finds code in simple list', () => {
       const listItems: ListItem[] = [
         {
-          'item-value': ['01'],
-          'item-description': ['Fastighetsägare'],
-          'item-definition': ['Ägare till fastighet'],
+          'item-value': '01',
+          'item-description': 'Fastighetsägare',
+          'item-definition': 'Ägare till fastighet',
         },
         {
-          'item-value': ['02'],
-          'item-description': ['Byggnadsägare'],
-          'item-definition': ['Ägare till byggnad'],
+          'item-value': '02',
+          'item-description': 'Byggnadsägare',
+          'item-definition': 'Ägare till byggnad',
         },
         {
-          'item-value': ['03'],
-          'item-description': ['Energiexpert'],
-          'item-definition': ['Av Boverket certifierad expert'],
+          'item-value': '03',
+          'item-description': 'Energiexpert',
+          'item-definition': 'Av Boverket certifierad expert',
         },
       ]
       const listItem = helper.findCodeInListItems('02', listItems)
       expect(listItem).toEqual({
-        'item-value': ['02'],
-        'item-description': ['Byggnadsägare'],
-        'item-definition': ['Ägare till byggnad'],
+        'item-value': '02',
+        'item-description': 'Byggnadsägare',
+        'item-definition': 'Ägare till byggnad',
       })
     })
 
     test('finds code in nested list', () => {
       const listItems: ListItem[] = [
         {
-          'item-value': ['01'],
-          'item-description': ['Fastighetsägare'],
-          'item-definition': ['Ägare till fastighet'],
+          'item-value': '01',
+          'item-description': 'Fastighetsägare',
+          'item-definition': 'Ägare till fastighet',
         },
         {
-          'item-value': ['02'],
-          'item-description': ['Byggnadsägare'],
-          'item-definition': ['Ägare till byggnad'],
+          'item-value': '02',
+          'item-description': 'Byggnadsägare',
+          'item-definition': 'Ägare till byggnad',
           'list-item': [
             {
-              'item-value': ['022'],
-              'item-description': ['Inner list item level 2'],
-              'item-definition': [''],
+              'item-value': '022',
+              'item-description': 'Inner list item level 2',
+              'item-definition': '',
               'list-item': [
                 {
-                  'item-value': ['0223'],
-                  'item-description': ['Inner list item level 3'],
-                  'item-definition': [''],
+                  'item-value': '0223',
+                  'item-description': 'Inner list item level 3',
+                  'item-definition': '',
                 },
               ],
             },
           ],
         },
         {
-          'item-value': ['03'],
-          'item-description': ['Energiexpert'],
-          'item-definition': ['Av Boverket certifierad expert'],
+          'item-value': '03',
+          'item-description': 'Energiexpert',
+          'item-definition': 'Av Boverket certifierad expert',
         },
       ]
       let listItem = helper.findCodeInListItems('022', listItems)
-      expect(listItem['item-description'][0]).toEqual('Inner list item level 2')
+      expect(listItem['item-description']).toEqual('Inner list item level 2')
 
       listItem = helper.findCodeInListItems('03', listItems)
-      expect(listItem['item-description'][0]).toEqual('Energiexpert')
+      expect(listItem['item-description']).toEqual('Energiexpert')
     })
 
     test('returns undefined when list is empty', () => {
