5c909bd4cf9143f7c95e719a8cef32ccbb9da0bc
martinloman <martin.loman@gmail.com>
2020-04-22 14:58:37 +0200

    feat: added response time

--- src/health.ts
+++ src/health.ts
@@ -1,16 +1,37 @@
 import { Application, Request, Response } from 'express'
 import tenantService from '@app/services/tenantservice'
+import { performance } from 'perf_hooks'
+
+interface SystemHealthInfo {
+  name: string
+  access: boolean
+  responseTime: number
+}
 
 interface HealthResponse {
-  fastAPIAccess: boolean
+  health: SystemHealthInfo[]
+}
+
+const checkFastAPIAccess = async (): Promise<SystemHealthInfo> => {
+  const start = performance.now()
+  const tenants = await tenantService.getTenants()
+  const end = performance.now()
+
+  const access = Array.isArray(tenants) && tenants.length > 0
+  const responseTime = end - start
+
+  return {
+    name: 'fastAPI',
+    access,
+    responseTime,
+  }
 }
 
 const checkHealth = async (): Promise<HealthResponse> => {
-  const tenants = await tenantService.getTenants()
-  const fastAPIAccess = Array.isArray(tenants) && tenants.length > 0
+  const health: SystemHealthInfo[] = [await checkFastAPIAccess()]
 
   return {
-    fastAPIAccess,
+    health,
   }
 }
 
