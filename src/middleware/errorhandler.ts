246d2ba0027a64836d77783b6e61b03104f32e52
martinloman <martin.loman@gmail.com>
2020-05-14 13:17:45 +0200

    fix: redacts information from errors

--- src/middleware/errorhandler.ts
+++ src/middleware/errorhandler.ts
@@ -1,15 +1,30 @@
 import { NextFunction, Request, Response } from 'express'
-import HttpException from '@app/commonTypes/exceptions'
 
-const errorMiddleware = (
-  error: HttpException,
-  request: Request,
-  response: Response,
-  next: NextFunction
-) => {
-  console.error(error)
+/**
+ * Redacts possibly sensitive information from errors before they are logged.
+ * @param error
+ */
+const redact = (error) => {
+  delete error.request
+  delete error.config
 
-  const status = error.status || 500
+  if (error.response) {
+    delete error.response.request
+    if (error.response.config) {
+      delete error.response.config.headers
+      if (error.response.config.url.includes('login')) {
+        error.response.config.url = 'login?***redacted**'
+      }
+    }
+  }
+  return error
+}
+
+const errorMiddleware = (error: any, request: Request, response: Response, next: NextFunction) => {
+  //TODO: Send error to logging service of choice.
+  console.error(redact(error))
+
+  const status = error ? error.response.status : 500
   const message = error.message || 'Something went wrong'
   response.status(status).send({
     status,
